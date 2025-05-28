d5c32117b2c8c8830910c629f6b8bf9a448d6168
Morteza Raeisi <mrraeisi@outlook.com>
2025-05-28 22:47:14 +0330

    remove the unnecessary IntoOrganizer trait

--- src/typ.rs
+++ src/typ.rs
@@ -1,19 +1,19 @@
 use std::time::Instant;
 
 #[derive(Clone, Debug)]
-pub struct CalendarEvent<'a, D: IntoTime, E: IntoEventOrganizer<'a>> {
+pub struct CalendarEvent<'a, D: IntoTime> {
     pub title: &'a str,
     pub start: D,
     pub url: Option<&'a str>,
     pub uid: Option<&'a str>,
     pub desc: Option<&'a str>,
     pub busy: Option<bool>,
-    pub stat: EventStatus,
+    pub stat: Option<EventStatus>,
     pub r_rule: Option<&'a str>,
     pub guests: Option<Vec<&'a str>>,
     pub location: Option<&'a str>,
     pub duration: EventDuration<D>,
-    pub organizer: Option<E>,
+    pub organizer: Option<EventOrganizer<'a>>,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -37,17 +37,6 @@ pub enum EventStatus {
 pub struct EventOrganizer<'a> {
     pub name: &'a str,
     pub email: &'a str,
-}
-pub trait IntoEventOrganizer<'a> {
-    fn into_event_organizer(self) -> EventOrganizer<'a>;
-}
-impl<'a> IntoEventOrganizer<'a> for (&'a str, &'a str) {
-    fn into_event_organizer(self) -> EventOrganizer<'a> {
-        EventOrganizer {
-            name: self.0,
-            email: self.1,
-        }
-    }
 }
 pub trait IntoTime {
     fn into_date(self) -> Instant;
