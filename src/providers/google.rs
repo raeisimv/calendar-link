21d0cc020cd10125ebe7b430831dbda90c39cbca
Morteza Raeisi <mrraeisi@outlook.com>
2025-06-01 20:55:51 +0330

    add unit test for description field

--- src/providers/google.rs
+++ src/providers/google.rs
@@ -10,12 +10,13 @@ pub fn google(event: CalendarEvent) -> MyResult<String> {
             Cow::Borrowed("dates"),
             Cow::Owned(format!("{}/{}", event.start, event.start + event.duration)),
         ),
-        (Cow::Borrowed("text"), Cow::Borrowed(event.title)),
     ];
 
     if let Some(x) = event.desc {
         p.push((Cow::Borrowed("details"), Cow::Borrowed(x)));
     }
+    p.push((Cow::Borrowed("text"), Cow::Borrowed(event.title)));
+
     if let Some(x) = event.busy {
         p.push((Cow::Borrowed("trp"), Cow::Owned(x.to_string())));
     }
@@ -62,6 +63,29 @@ mod tests {
         assert_eq!(
             link.as_str(),
             "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20191229T000000Z%2F20191229T020000Z&text=Birthday%20party".replace("%20","+")
+        );
+    }
+    #[test]
+    fn should_create_google_calendar_link_with_desc() {
+        let date = chrono::DateTime::parse_from_rfc3339("2019-12-29T00:00:00Z").unwrap();
+        let evt = CalendarEvent {
+            title: "Birthday party",
+            start: EventTime::DateTime(TimeType::Utc(date.into())),
+            duration: EventDuration::For(Duration::hours(2)),
+            url: None,
+            uid: None,
+            desc: Some("Bring gifts!"),
+            busy: None,
+            stat: None,
+            r_rule: None,
+            guests: None,
+            location: None,
+            organizer: None,
+        };
+        let link = google(evt).unwrap();
+        assert_eq!(
+            link.as_str(),
+            "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20191229T000000Z%2F20191229T020000Z&details=Bring%20gifts%21&text=Birthday%20party".replace("%20","+")
         );
     }
 }
