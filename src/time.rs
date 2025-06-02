0243ee1aed64a1d306d8c7ac780bb703de123ddb
Morteza Raeisi <mrraeisi@outlook.com>
2025-06-02 22:30:09 +0330

    use native time formatter

--- src/time.rs
+++ src/time.rs
@@ -1,15 +1,12 @@
-use crate::typ::EventDuration;
-use chrono::{DateTime, Duration, Local, Utc};
-use core::{
-    fmt::{Display, Formatter},
-    ops::Add,
-};
+use chrono::{DateTime, Local, Utc};
+use core::fmt::{Display, Formatter};
 
 #[derive(Debug, Clone, Copy)]
 pub enum EventTime {
     Utc(DateTime<Utc>),
     Local(DateTime<Local>),
 }
+
 impl EventTime {
     #[cfg(test)]
     pub fn fixed_utc_time() -> DateTime<Utc> {
@@ -23,15 +20,8 @@ impl EventTime {
         EventTime::Utc(Self::fixed_utc_time())
     }
 
-    pub fn format_as_str(&self, date_only: bool) -> String {
-        let fmt = if date_only {
-            "%Y%m%d"
-        } else {
-            match self {
-                EventTime::Utc(_) => "%Y%m%dT%H%M%SZ",
-                EventTime::Local(_) => "%Y-%m-%dT%H:%M:%S",
-            }
-        };
+    pub fn format_as_string(&self, format: EventTimeFormat) -> String {
+        let fmt = format.as_ref();
         match self {
             EventTime::Utc(x) => x.format(fmt).to_string(),
             EventTime::Local(x) => x.format(fmt).to_string(),
@@ -45,27 +35,22 @@ impl Default for EventTime {
 }
 impl Display for EventTime {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.format_as_str(false))
+        write!(f, "{}", self.format_as_string(EventTimeFormat::DateTimeUtc))
     }
 }
 
-impl Add<EventDuration> for EventTime {
-    type Output = EventTime;
-
-    fn add(self, rhs: EventDuration) -> Self::Output {
-        use EventTime::*;
-
+#[derive(Debug, Clone, Copy)]
+pub enum EventTimeFormat {
+    AllDay,
+    DateTimeUtc,
+    DateTimeLocal,
+}
+impl AsRef<str> for EventTimeFormat {
+    fn as_ref(&self) -> &str {
         match self {
-            Utc(x) => match rhs {
-                EventDuration::AllDay => Utc(x.add(Duration::days(1))),
-                EventDuration::For(dur) => Utc(x.add(dur)),
-                EventDuration::EndsAt(d) => d,
-            },
-            Local(x) => match rhs {
-                EventDuration::AllDay => Local(x.add(Duration::days(1))),
-                EventDuration::For(dur) => Local(x.add(dur)),
-                EventDuration::EndsAt(d) => d,
-            },
+            EventTimeFormat::AllDay => "%Y%m%d",
+            EventTimeFormat::DateTimeUtc => "%Y%m%dT%H%M%SZ",
+            EventTimeFormat::DateTimeLocal => "%Y-%m-%dT%H:%M:%S",
         }
     }
 }
@@ -73,7 +58,6 @@ impl Add<EventDuration> for EventTime {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::typ::EventDuration;
 
     #[test]
     fn should_format_utc() {
@@ -82,7 +66,7 @@ mod tests {
         assert_eq!(x.to_string(), "20191228T120000Z");
 
         let x = EventTime::Utc(d);
-        assert_eq!(x.format_as_str(true), "20191228");
+        assert_eq!(x.format_as_string(EventTimeFormat::AllDay), "20191228");
     }
     #[test]
     fn should_format_local() {
@@ -91,21 +75,6 @@ mod tests {
         assert_eq!(x.to_string(), "2019-12-28T15:30:00"); // it may not pass on your system
 
         let x = EventTime::Local(d.into());
-        assert_eq!(x.format_as_str(true), "20191228");
-    }
-    #[test]
-    fn should_sum_up_with_event_duration() {
-        let start = EventTime::fixed_utc();
-        let dur = EventDuration::AllDay;
-        let end = start + dur;
-        assert_eq!(end.to_string(), "20191229T120000Z");
-
-        let dur = EventDuration::For(Duration::hours(1));
-        let end = start + dur;
-        assert_eq!(end.to_string(), "20191228T130000Z");
-
-        let dur = EventDuration::at(EventTime::fixed_utc_time().add(Duration::hours(2)));
-        let end = start + dur;
-        assert_eq!(end.to_string(), "20191228T140000Z");
+        assert_eq!(x.format_as_string(EventTimeFormat::AllDay), "20191228");
     }
 }
