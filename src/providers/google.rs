85a3e5150eb365a2abf80dc1dc26290dbe6cc652
Morteza Raeisi <mrraeisi@outlook.com>
2025-06-06 19:51:12 +0330

    [: save works :]

--- src/providers/google.rs
+++ src/providers/google.rs
@@ -1,15 +1,15 @@
 use crate::{err::MyResult, stringify::make_url, typ::CalendarEvent};
 use std::borrow::Cow;
 
-pub fn google(event: CalendarEvent) -> MyResult<String> {
+pub fn google(event: &CalendarEvent) -> MyResult<String> {
     let mut p = vec![(Cow::Borrowed("action"), Cow::Borrowed("TEMPLATE"))];
 
-    let date_only = event.duration.is_all_day();
+    // let date_only = event.duration.is_all_day();
     p.push((
         Cow::Borrowed("dates"),
         Cow::Owned(format!(
             "{}/{}",
-            event.start.format_as_str(date_only),
+            event.start.format_as_string(date_only),
             (event.start + event.duration).format_as_str(date_only)
         )),
     ));
@@ -26,7 +26,7 @@ pub fn google(event: CalendarEvent) -> MyResult<String> {
     if let Some(x) = event.r_rule {
         p.push((Cow::Borrowed("recur"), Cow::Owned(format!("RRULE: {}", x))));
     }
-    if let Some(x) = event.guests {
+    if let Some(x) = &event.guests {
         if !x.is_empty() {
             p.push((Cow::Borrowed("add"), Cow::Owned(x.join(","))));
         }
@@ -40,78 +40,18 @@ pub fn google(event: CalendarEvent) -> MyResult<String> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::time::EventTime;
-    use crate::typ::EventDuration;
-    use chrono::Duration;
+    use crate::prelude::google;
+    use crate::providers::__snapshot__::{generate_models, read_snapshot};
 
     #[test]
-    fn should_create_google_calendar_link() {
-        let date = chrono::DateTime::parse_from_rfc3339("2019-12-29T00:00:00Z").unwrap();
-        let evt = CalendarEvent {
-            title: "Birthday party",
-            start: EventTime::Utc(date.into()),
-            duration: EventDuration::For(Duration::hours(2)),
-            url: None,
-            uid: None,
-            desc: None,
-            busy: None,
-            stat: None,
-            r_rule: None,
-            guests: None,
-            location: None,
-            organizer: None,
-        };
-        let link = google(evt).unwrap();
-        assert_eq!(
-            link.as_str(),
-            "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20191229T000000Z%2F20191229T020000Z&text=Birthday%20party".replace("%20","+")
-        );
-    }
-    #[test]
-    fn should_create_google_calendar_link_with_desc() {
-        let date = chrono::DateTime::parse_from_rfc3339("2019-12-29T00:00:00Z").unwrap();
-        let evt = CalendarEvent {
-            title: "Birthday party",
-            start: EventTime::Utc(date.into()),
-            duration: EventDuration::For(Duration::hours(2)),
-            url: None,
-            uid: None,
-            desc: Some("Bring gifts!"),
-            busy: None,
-            stat: None,
-            r_rule: None,
-            guests: None,
-            location: None,
-            organizer: None,
-        };
-        let link = google(evt).unwrap();
-        assert_eq!(
-            link.as_str(),
-            "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20191229T000000Z%2F20191229T020000Z&details=Bring%20gifts%21&text=Birthday%20party".replace("%20","+")
-        );
-    }
-    #[test]
-    fn should_create_google_calendar_link_all_day() {
-        let date = chrono::DateTime::parse_from_rfc3339("2019-12-29T00:00:00Z").unwrap();
-        let evt = CalendarEvent {
-            title: "Birthday party",
-            start: EventTime::Utc(date.into()),
-            duration: EventDuration::AllDay,
-            url: None,
-            uid: None,
-            desc: None,
-            busy: None,
-            stat: None,
-            r_rule: None,
-            guests: None,
-            location: None,
-            organizer: None,
-        };
-        let link = google(evt).unwrap();
-        assert_eq!(
-            link.as_str(),
-            "https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20191229%2F20191230&text=Birthday%20party".replace("%20","+")
-        );
+    fn should_provide_google_calendar_link() {
+        let snapshot = read_snapshot();
+        let models = generate_models();
+        let mut cases = snapshot.get("google").unwrap().into_iter();
+        for (i, evt) in models.iter().enumerate() {
+            let act = google(evt).expect("cannot parse google event");
+            let exp = cases.next().expect("sequence contains no elements");
+            assert_eq!(&act, exp, "failed at index {i}, evt: {evt:?}");
+        }
     }
 }
