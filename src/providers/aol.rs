763e75e8909d79b4e28d19c3e925eaf77dbc2c91
Morteza Raeisi <mrraeisi@outlook.com>
2025-06-12 21:27:16 +0330

    impl AOL calendar link generator

---
+++ src/providers/aol.rs
@@ -0,0 +1,58 @@
+use crate::{err::MyResult, stringify::make_url, time::EventTimeFormat, typ::CalendarEvent};
+use std::borrow::Cow;
+
+pub fn aol(event: &CalendarEvent) -> MyResult<String> {
+    let mut p = vec![
+        (Cow::Borrowed("v"), Cow::Borrowed("60")),
+        (Cow::Borrowed("title"), Cow::Borrowed(event.title)),
+    ];
+
+    let fmt_typ = if event.is_all_day() {
+        EventTimeFormat::AllDay
+    } else {
+        EventTimeFormat::DateTimeUtc
+    };
+    p.push((
+        Cow::Borrowed("st"),
+        Cow::Owned(event.start.format_as_string(fmt_typ)),
+    ));
+    p.push((
+        Cow::Borrowed("et"),
+        Cow::Owned(event.end_date().format_as_string(fmt_typ)),
+    ));
+
+    if let Some(x) = event.desc {
+        p.push((Cow::Borrowed("desc"), Cow::Borrowed(x)));
+    }
+    if let Some(x) = event.location {
+        p.push((Cow::Borrowed("in_loc"), Cow::Borrowed(x)));
+    }
+    if event.is_all_day() {
+        p.push((Cow::Borrowed("dur"), Cow::Borrowed("allday")));
+    } else {
+        p.push((Cow::Borrowed("dur"), Cow::Borrowed("false")));
+    }
+
+    make_url(
+        "https://calendar.aol.com",
+        p.iter().map(|(x, y)| (x.as_ref(), y.as_ref())),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::providers::__snapshot__::{generate_models, read_snapshot};
+
+    #[test]
+    fn should_provide_aol_calendar_link() {
+        let snapshot = read_snapshot();
+        let models = generate_models();
+        let mut cases = snapshot.get("aol").unwrap().into_iter();
+        for (i, evt) in models.iter().enumerate() {
+            let act = aol(evt).expect("cannot parse aol event");
+            let exp = cases.next().expect("sequence contains no elements");
+            assert_eq!(&act, exp, "failed at index {i}, evt: {evt:?}");
+        }
+    }
+}
--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,9 +1,11 @@
 #[cfg(test)]
 mod __snapshot__;
+mod aol;
 mod google;
 mod outlook;
 mod yahoo;
 
+pub use aol::*;
 pub use google::*;
 pub use outlook::*;
 pub use yahoo::*;
