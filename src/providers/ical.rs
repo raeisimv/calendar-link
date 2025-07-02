4809e8248af52803d7f6aad9c19a22476cdeba84
Morteza Raeisi <mrraeisi@outlook.com>
2025-07-02 19:12:30 +0330

    add iCal skeleton

---
+++ src/providers/ical.rs
@@ -0,0 +1,58 @@
+use crate::{err::MyResult, time::EventTimeFormat, typ::CalendarEvent, url::URL};
+use std::borrow::Cow;
+
+pub fn ical(event: &CalendarEvent) -> MyResult<URL> {
+    let mut p = vec![
+        (Cow::Borrowed("v"), Cow::Borrowed("60")),
+        (Cow::Borrowed("title"), Cow::Borrowed(event.title)),
+    ];
+
+    let fmt_typ = if event.is_all_day() {
+        EventTimeFormat::AllDay
+    } else {
+        EventTimeFormat::DateTimeUtc
+    };
+    p.push((
+        Cow::Borrowed("st"),
+        Cow::Owned(event.start.format_as_string(fmt_typ)),
+    ));
+    p.push((
+        Cow::Borrowed("et"),
+        Cow::Owned(event.end_date().format_as_string(fmt_typ)),
+    ));
+
+    if let Some(x) = event.desc {
+        p.push((Cow::Borrowed("desc"), Cow::Borrowed(x)));
+    }
+    if let Some(x) = event.location {
+        p.push((Cow::Borrowed("in_loc"), Cow::Borrowed(x)));
+    }
+    if event.is_all_day() {
+        p.push((Cow::Borrowed("dur"), Cow::Borrowed("allday")));
+    } else {
+        p.push((Cow::Borrowed("dur"), Cow::Borrowed("false")));
+    }
+
+    URL::try_build(
+        "https://calendar.ical.com",
+        p.iter().map(|(x, y)| (x.as_ref(), y.as_ref())),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::providers::__snapshot__::{generate_models, read_snapshot};
+
+    #[test]
+    fn should_provide_ical_calendar_link() {
+        let snapshot = read_snapshot();
+        let models = generate_models();
+        let mut cases = snapshot.get("ical").unwrap().into_iter();
+        for (i, evt) in models.iter().enumerate() {
+            let act = ical(evt).expect("cannot parse ical event");
+            let exp = cases.next().expect("sequence contains no elements");
+            assert_eq!(act, URL::new(exp), "failed at index {i}, evt: {evt:?}");
+        }
+    }
+}
--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -2,12 +2,14 @@
 mod __snapshot__;
 mod aol;
 mod google;
+mod ical;
 mod msteams;
 mod outlook;
 mod yahoo;
 
 pub use aol::*;
 pub use google::*;
+pub use ical::*;
 pub use msteams::*;
 pub use outlook::*;
 pub use yahoo::*;
