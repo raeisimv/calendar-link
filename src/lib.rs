75eefaa7691e7662646ae1553a4794a63640ea5b
Morteza Raeisi <mrraeisi@outlook.com>
2025-07-05 21:59:31 +0330

    update documentations  - inc version

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+///! # Calendar Link
+///! this is a crate about generating URL links for various calendar services including Google Calendar, iCalendar, Outlook, etc.
+///!
 pub mod err;
 pub mod prelude;
 pub mod providers;
