feed6cec8ae97e0efa57fb5f6d9255b03020d000
Morteza Raeisi <mrraeisi@outlook.com>
2025-07-17 23:19:08 +0330

    disable the ics for now  - inc version

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -2,14 +2,14 @@
 mod __snapshot__;
 mod aol;
 mod google;
-mod ics;
+// mod ics;
 mod msteams;
 mod outlook;
 mod yahoo;
 
 pub use aol::*;
 pub use google::*;
-pub use ics::*;
+// pub use ics::*;
 pub use msteams::*;
 pub use outlook::*;
 pub use yahoo::*;
