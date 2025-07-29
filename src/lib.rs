332925a9671930f0d2879b7797f212771aa17d6e
Morteza Raeisi <raeisimv@gmail.com>
2025-07-29 17:02:56 +0330

    add notice message

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 ///! # Calendar Link
 ///! this is a crate about generating URL links for various calendar services including Google Calendar, iCalendar, Outlook, etc.
-///!
+///! This crate is in its early stages and is not production ready yet.
 pub mod err;
 pub mod prelude;
 pub mod providers;
