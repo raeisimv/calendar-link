a257480fa645fcc527c69165fb08576c730e1138
Morteza Raeisi <mrraeisi@outlook.com>
2025-08-02 20:02:16 +0330

    remove MS teams

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -3,13 +3,13 @@ mod __snapshot__;
 mod aol;
 mod google;
 mod ics;
-mod msteams;
+// mod msteams;
 mod outlook;
 mod yahoo;
 
 pub use aol::*;
 pub use google::*;
 pub use ics::*;
-pub use msteams::*;
+// pub use msteams::*;
 pub use outlook::*;
 pub use yahoo::*;
